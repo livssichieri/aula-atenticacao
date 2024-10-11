d351cbb4dd0ba0c5b65ca65ffb99cfecaae76437
liviasicchieri <liviacarolinasouzasicchieri@gmail.com>
2024-10-11 14:08:19 -0300

    desafio

--- interface/src/routes.jsx
+++ interface/src/routes.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Pessoa from './pages/Pessoa/Pessoa';
 import Login from './pages/Login/Login';
+import ProtectedRoute from './components/Rotas/ProtectedRoutes';
 
 function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path='/pessoas' element={<Pessoa />} />
+                <Route path='/pessoas' element={<ProtectedRoute element={Pessoa} />} />
                 <Route path="/login" element={<Login />} />
 
             </Routes>
